a0c3b6f104b4a440819a8b26d4cde11d44e47132
vvarchioni <v.varchioni@gmail.com>
2021-04-21 17:13:01 -0300

    template login presentado sin funcionalidades

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from resources import usuario
 
 app = Flask (__name__)
 
-
-app.add_url_rule("/register", "userRegister", usuario.userRegister)
+#Usuarios
+app.add_url_rule("/login", "login", usuario.login)
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/resources/usuario.py
+++
@@ -1,6 +0,0 @@
-import flask from render_template
-
-
-def userRegister ():
-
-    return render_template ("register.html")

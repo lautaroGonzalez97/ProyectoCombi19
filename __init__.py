ab3725c95ad3b64935f7bd61454aa328498efc99
vvarchioni <v.varchioni@gmail.com>
2021-04-22 18:02:32 -0300

    se agregaron rutas para login, autenticar y logout

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import render_template, redirect, url_for, session
 from resources import usuario
 
 app = Flask (__name__)
 
 #Usuarios
-app.add_url_rule("/login", "login", usuario.login)
+app.add_url_rule("/iniciar_sesion","login", usuario.login)
+app.add_url_rule("/autenticacion", "autenticar_login", usuario.autenticar, methods=["POST"])
+app.add_url_rule("/cerrar_sesion", "logout", usuario.logout)
 
+#Esto es para cuando ingresan a la pagina, si su id no esta en Session los tira al template login, sino entran a la pagina (#)
 @app.route('/')
 def home():
-    return redirect(url_for('login'))
+    if (not 'id' in session):
+        return redirect(url_for("login"))
+    return redirect(url_for('#')) #redirije a home
 
 if __name__ == '__main__':
     app.run(debug=True)

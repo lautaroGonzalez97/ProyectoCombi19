4d228a401a0def5697118bf5aee284ce0898cb00
vvarchioni <v.varchioni@gmail.com>
2021-04-23 13:13:24 -0300

    probar conexion con bd

--- __init__.py
+++ __init__.py
@@ -1,19 +1,42 @@
-from flask import render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
+from flask_mysqldb import MySQL
 from resources import usuario
 
 app = Flask (__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "combi_19"
+mysql = MySQL(app)
 
 #Usuarios
 app.add_url_rule("/iniciar_sesion","login", usuario.login)
-app.add_url_rule("/autenticacion", "autenticar_login", usuario.autenticar, methods=["POST"])
-app.add_url_rule("/cerrar_sesion", "logout", usuario.logout)
+#app.add_url_rule("/autenticacion", "autenticar_login", usuario.autenticar, methods=["POST"])
+#app.add_url_rule("/cerrar_sesion", "logout", usuario.logout)
+#app.add_url_rule("/registrar", "registrar", usuario.registrar, methods=["POST"])
+
 
 #Esto es para cuando ingresan a la pagina, si su id no esta en Session los tira al template login, sino entran a la pagina (#)
 @app.route('/')
-def home():
-    if (not 'id' in session):
+#def home():
+#    if (not 'id' in session):
+#        return redirect(url_for("login"))
+#    return redirect(url_for('#')) #redirije a home
+
+@app.route("/")
+def home ():
+    return render_template("registrar.html")
+
+@app.route("/registrar")
+def registrar():
+    if (request.method == "POST"):
+        datos = request.form
+        email = datos["email"]
+        password = datos["password"]
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO usuarios (email, password) VALUES (%s, %s)", (email, password))
+        mysql.connection.commit()
         return redirect(url_for("login"))
-    return redirect(url_for('#')) #redirije a home
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port= 8080, debug=True)
--- resources/usuario.py
+++ resources/usuario.py
@@ -1,24 +1,24 @@
 from flask import render_template, session, redirect, url_for, flash, request
 from helpers.auth import authenticated
 
-
 def login():
-    if (authenticated(session)):
-        return redirect(url_for("#")) #redirije a home
+#    if (authenticated(session)):
+#        return redirect(url_for("#")) #redirije a home
     return render_template("login.html")
 
-def autenticar():
-    datos = request.form 
-    email = datos["email"]
-    password = datos["password"]
-    idUser = #buscar usuario en la bd que coincida con email y password y me devuelve el campo id
-    if (idUser is not None):
-        session["id"] = idUser
-    else:
-        flash ("Correo o clave incorrecta", "error")
-        return redirect(url_for("login"))
+#def autenticar():
+#    datos = request.form 
+#    email = datos["email"]
+#    password = datos["password"]
+    #idUser = buscar usuario en la bd que coincida con email y password y me devuelve el campo id
+#    if (idUser is not None):
+#        session["id"] = idUser
+#    else:
+#        flash ("Correo o clave incorrecta", "error")
+#        return redirect(url_for("login")
+#    return redirect(url_for("#") #redirije a home
 
-def logout():
-    if(authenticated(session)):
-        del session["id"]
-    return redirect(url_for("login"))
+#def logout():
+#    if(authenticated(session)):
+#        del session["id"]
+#    return redirect(url_for("login"))

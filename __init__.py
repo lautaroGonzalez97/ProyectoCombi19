cc5834523d2d2b98431233fbf27464a16826f85b
vvarchioni <v.varchioni@gmail.com>
2021-04-23 16:55:07 -0300

    funcionalidad login correcta y funcionalidad registrar correcta pero con datos inventados

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, session, request
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, redirect, url_for, session, request, flash
 from resources import usuario
+from flask_mysqldb import MySQL
 
 app = Flask (__name__)
 app.config["MYSQL_HOST"] = "localhost"
@@ -9,7 +9,9 @@ app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "combi_19"
 mysql = MySQL(app)
 
-#Usuarios
+app.secret_key = 'esto-es-una-clave-muy-secreta'
+
+#Rutas Usuarios
 app.add_url_rule("/iniciar_sesion","login", usuario.login)
 #app.add_url_rule("/autenticacion", "autenticar_login", usuario.autenticar, methods=["POST"])
 #app.add_url_rule("/cerrar_sesion", "logout", usuario.logout)
@@ -17,15 +19,15 @@ app.add_url_rule("/iniciar_sesion","login", usuario.login)
 
 
 #Esto es para cuando ingresan a la pagina, si su id no esta en Session los tira al template login, sino entran a la pagina (#)
-@app.route('/')
-#def home():
-#    if (not 'id' in session):
-#        return redirect(url_for("login"))
-#    return redirect(url_for('#')) #redirije a home
+'''@app.route('/')
+def home():
+    if (not 'id' in session):
+        return redirect(url_for("login"))
+    return redirect(url_for('#')) #redirije a home'''
 
 @app.route("/")
 def home ():
-    return render_template("reg.html")
+    return render_template("login.html")
 
 @app.route("/registrar" , methods=["POST"])
 def registrar():
@@ -36,6 +38,21 @@ def registrar():
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO usuario (email, password) VALUES (%s, %s)", (email, password))
         mysql.connection.commit()
+        return redirect(url_for("login"))
+
+@app.route("/autenticar" , methods=["POST"])
+def autenticar ():
+    datos = request.form 
+    email = datos["email"]
+    password = datos["password"]
+    cur = mysql.connection.cursor()
+    idUser = cur.execute("SELECT id FROM usuario WHERE (email = %s) and (password = %s)", (email, password))
+    if (idUser != 0):
+        session["id"] = idUser
+        return render_template("home.html")
+    else:
+        print("entro mal los datos")
+        flash ("Correo o clave incorrecta") #no muestra mensaje flash, porque?
         return redirect(url_for("login"))
 
 if __name__ == '__main__':
--- resources/usuario.py
+++ resources/usuario.py
@@ -6,19 +6,19 @@ def login():
 #        return redirect(url_for("#")) #redirije a home
     return render_template("login.html")
 
-#def autenticar():
-#    datos = request.form 
-#    email = datos["email"]
-#    password = datos["password"]
-    #idUser = buscar usuario en la bd que coincida con email y password y me devuelve el campo id
-#    if (idUser is not None):
-#        session["id"] = idUser
-#    else:
-#        flash ("Correo o clave incorrecta", "error")
-#        return redirect(url_for("login")
-#    return redirect(url_for("#") #redirije a home
+'''def autenticar():
+    datos = request.form 
+    email = datos["email"]
+    password = datos["password"]
+    idUser = buscar usuario en la bd que coincida con email y password y me devuelve el campo id
+    if (idUser is not None):
+        session["id"] = idUser
+    else:
+        flash ("Correo o clave incorrecta", "error")
+        return redirect(url_for("login")
+    return redirect(url_for("#") #redirije a home
 
-#def logout():
-#    if(authenticated(session)):
-#        del session["id"]
-#    return redirect(url_for("login"))
+def logout():
+    if(authenticated(session)):
+        del session["id"]
+    return redirect(url_for("login"))'''
